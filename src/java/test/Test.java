344751297dfacfeca1e4342b8013f66e5ce0ff76
EmmanuelSantanaISA <jairoemmanuelsantana@gmail.com>
2016-04-11 14:36:57 -0500

    Added FarmService

--- src/java/test/Test.java
+++ src/java/test/Test.java
@@ -5,13 +5,28 @@
  */
 package test;
 
+import dao.FarmDao;
+import pojo.Farm;
+import pojo.Line;
+import pojo.Reader;
+import pojo.Sensor;
+import pojo.SensorType;
+import pojo.Ship;
+
 /**
  *
  * @author emman
  */
 public class Test {
     public static void main(String[] args) {
+        FarmDao farmDao = new FarmDao();
+        Farm farm = new Farm();
+        Ship newShip = new Ship();
+        Line newLine = new Line();
+        Reader newReader = new Reader();
+        Sensor tempSensor = new Sensor();
         
+        farmDao.create(farm);
     }
    
 }
